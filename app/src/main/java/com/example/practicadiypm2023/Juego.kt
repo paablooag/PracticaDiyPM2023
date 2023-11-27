7197430f4d23adbf8789160fd9462bf34e423f57
EAG <pablo.alcalde.garcia@gmail.com>
2023-11-27 18:16:01 +0100

    ñlk

--- app/src/main/java/com/example/practicadiypm2023/Juego.kt
+++ app/src/main/java/com/example/practicadiypm2023/Juego.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.ImageView
+import android.widget.TextView
 import kotlin.concurrent.thread
 import kotlin.concurrent.timer
 import kotlin.random.Random
@@ -15,6 +17,7 @@ class Juego : AppCompatActivity() {
     private lateinit var imagenes:MutableList<Int>
     private lateinit var booleanos:MutableList<Boolean>
 
+    var contadorGanar = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
@@ -24,9 +27,14 @@ class Juego : AppCompatActivity() {
                                     ,R.drawable.mujerdurmiendo, R.drawable.mujervenus, R.drawable.mujerpelirroja)
 
         booleanos = mutableListOf<Boolean>(false, false, false, false, false, false, false, false, false, false, false, false)
+        imagenes.shuffle()
+
+
     }
     lateinit var cartaActual:ImageView
     lateinit var cartaAnterior:ImageView
+    var posicionAnterior:Int? = null
+    var contador = 0
 
     fun cartaPulsada(view: View) {
         if (::cartaActual.isInitialized) {
@@ -34,153 +42,63 @@ class Juego : AppCompatActivity() {
         }
         cartaActual = view as ImageView
 
-
-        when(view.id){
-            R.id.carta1->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta1)
-                if(!booleanos[0]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-                    booleanos[0]=true
-                }
-            }
-            R.id.carta2->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta2)
-                if(!booleanos[1]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-                    booleanos[1]=true
-                }
-            }
-            R.id.carta3->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta3)
-                if(!booleanos[2]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[2]=true
-                }
-            }
-            R.id.carta4->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta4)
-                if(!booleanos[3]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[3]=true
-                }
-            }
-            R.id.carta5->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta5)
-                if(!booleanos[4]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[4]=true
-                }
-            }
-            R.id.carta6->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta6)
-                if(!booleanos[5]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[5]=true
-                }
-            }
-            R.id.carta7->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta7)
-                if(!booleanos[6]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[6]=true
-                }
-            }
-            R.id.carta8->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta8)
-                if(!booleanos[7]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[7]=true
-                }
-            }
-            R.id.carta9->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta9)
-                if(!booleanos[8]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[8]=true
-                }
-            }
-            R.id.carta10->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta10)
-                if(!booleanos[9]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[9]=true
-                }
-            }
-            R.id.carta11->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta11)
-                if(!booleanos[10]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[10]=true
-                }
-            }
-            R.id.carta12->{
-                var cartanueva1 = findViewById<ImageView>(R.id.carta12)
-                if(!booleanos[11]){
-                    var img_nueva = imagenes.random()
-                    cartanueva1.setImageResource(img_nueva)
-                    cartanueva1.tag = img_nueva.toString()
-                    imagenes.remove(img_nueva)
-
-                    booleanos[11]=true
-                }
-            }
+        val posicion = when (view.id) {
+            R.id.carta1 -> 0
+            R.id.carta2 -> 1
+            R.id.carta3 -> 2
+            R.id.carta4 -> 3
+            R.id.carta5 -> 4
+            R.id.carta6 -> 5
+            R.id.carta7 -> 6
+            R.id.carta8 -> 7
+            R.id.carta9 -> 8
+            R.id.carta10 -> 9
+            R.id.carta11 -> 10
+            R.id.carta12 -> 11
+            else -> -1
         }
 
-        if (::cartaAnterior.isInitialized) {
-            compararCartas(cartaAnterior, cartaActual)
+        if (!booleanos[posicion]) {
+            contador++
+            val cartanueva1 = view.findViewById<ImageView>(view.id)
 
+            cartanueva1.setImageResource(imagenes[posicion])
+            cartanueva1.tag = imagenes[posicion].toString()
+
+            booleanos[posicion] = true
+        }
+
+        if (::cartaAnterior.isInitialized && contador == 2) {
+            compararCartas(cartaAnterior, cartaActual, posicion, posicionAnterior!!)
+        } else {
+            posicionAnterior = posicion
         }
     }
-    private fun compararCartas(cartaAnterior: ImageView, cartaActual: ImageView) {
+    private fun compararCartas(cartaAnterior: ImageView, cartaActual: ImageView, posicion:Int, posicionAnterior:Int) {
         val idAnterior = cartaAnterior.tag
         val idActual = cartaActual.tag
-        if (idAnterior != idActual) {
+        var posicion = posicion
+        var posicionAnterior = posicionAnterior
+
+        if (idAnterior != idActual && contador==2) {
             cartaAnterior.postDelayed({
                 cartaAnterior.setImageResource(R.drawable.parteatras)
                 cartaActual.setImageResource(R.drawable.parteatras)
-            }, 1000)
+                booleanos[posicion]=false
+                booleanos[posicionAnterior]=false
+                contador=0
+            }, 500)
+        }else if(contador==2){
+            booleanos[posicion]=true
+            booleanos[posicionAnterior]=true
+            cartaAnterior.tag=null
+            contador=0
+            posicionAnterior=0
+            contadorGanar++
+            if(contadorGanar==6){
+                var texto = findViewById<TextView>(R.id.textGanar)
+                texto.setText("Has ganado campeón")
+            }
         }
     }
 
