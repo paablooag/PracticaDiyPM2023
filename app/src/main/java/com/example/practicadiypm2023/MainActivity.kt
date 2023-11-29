7616471fd27dcd76a81244f752c4340c26798a39
EAG <pablo.alcalde.garcia@gmail.com>
2023-11-29 18:26:16 +0100

    asdasdadas

--- app/src/main/java/com/example/practicadiypm2023/MainActivity.kt
+++ app/src/main/java/com/example/practicadiypm2023/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.practicadiypm2023
 
 import android.content.Intent
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView : AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) { }
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     fun Iniciar(view: View) {
