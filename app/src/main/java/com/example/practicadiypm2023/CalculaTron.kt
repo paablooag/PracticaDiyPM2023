7c6a869d0c0b6120b48b29f4a372b82c3a3242fc
EAG <pablo.alcalde.garcia@gmail.com>
2023-12-01 19:47:11 +0100

    gay

--- app/src/main/java/com/example/practicadiypm2023/CalculaTron.kt
+++ app/src/main/java/com/example/practicadiypm2023/CalculaTron.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.view.View
 import android.widget.TextView
 import java.util.Random
 
@@ -13,7 +14,7 @@ class CalculaTron : AppCompatActivity() {
     private lateinit var operacionAzar: TextView
     private lateinit var operacionSiguiente:TextView
     private lateinit var operacionAntigua:TextView
-    private var igualPulsado = false
+    private lateinit var tutexta:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,55 +22,164 @@ class CalculaTron : AppCompatActivity() {
         setContentView(R.layout.activity_calcula_tron)
         temporizadorTextView = findViewById(R.id.temporizador)
         operacionAntigua = findViewById(R.id.operacionAntigua2)
+        tutexta=findViewById(R.id.tutexta)
 
         empezarTemporizador()
 
         operacionAzar = findViewById(R.id.operacionAzar)
         operacionSiguiente=findViewById(R.id.operacionSiguiente)
 
-        generarSuma()
-        generarOperacionSiguiente()
-        operacionAntigua()
-    }
+        val num1 = Random().nextInt(10)
+        val num2 = Random().nextInt(10)
 
+        val operacion = "$num1 + $num2"
+        operacionAzar.text = operacion
+
+        generarOperacionSiguiente()
+
+    }
+    var res=""
+    var proxres=""
+    fun botonNumero(view:View){
+        when (view.id) {
+            R.id.cero -> {
+                res += "0"
+                tutexta.text = res
+            }
+
+            R.id.uno -> {
+                res += "1"
+                tutexta.text = res
+            }
+
+            R.id.dos -> {
+                res += "2"
+                tutexta.text = res
+            }
+
+            R.id.tres -> {
+                res += "3"
+                tutexta.text = res
+            }
+
+            R.id.cuatro -> {
+                res += "4"
+                tutexta.text = res
+            }
+
+            R.id.cinco -> {
+                res += "5"
+                tutexta.text = res
+            }
+
+            R.id.seis -> {
+                res += "6"
+                tutexta.text = res
+            }
+
+            R.id.siete -> {
+                res += "7"
+                tutexta.text = res
+            }
+
+            R.id.ocho -> {
+                res += "8"
+                tutexta.text = res
+            }
+
+            R.id.nueve -> {
+                res += "9"
+                tutexta.text = res
+            }
+
+            R.id.c -> {
+                if (res != "") {
+                    res = res.substring(0, res.length - 1)
+                }
+                tutexta.text = res
+
+            }
+
+            R.id.ce -> {
+                res = ""
+                tutexta.text = res
+            }
+
+            R.id.igual -> {
+                operacionAntigua.text=operacionAzar.text
+                operacionAzar.text=operacionSiguiente.text
+                proxres=res
+                res=""
+                generarOperacionSiguiente()
+            }
+        }
+    }
     private fun empezarTemporizador() {
         temporizador = object : CountDownTimer(45000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 temporizadorTextView.text = (millisUntilFinished / 1000).toString()
-
             }
             override fun onFinish() {
-                temporizadorTextView.text = "El tiempo ha terminado"
+                temporizadorTextView.text = "Tiempo acabado"
                 Handler().postDelayed({
                     recreate()
                 },3000)
             }
         }
-
         temporizador?.start()
-
     }
-
-
-    private fun generarSuma() {
-
-        val random = Random()
-        val num1 = random.nextInt(10)
-        val num2 = random.nextInt(10)
-
-        val operacion = "$num1 + $num2"
-        operacionAzar.text = operacion
-    }
-
     private fun generarOperacionSiguiente(){
-        val random = Random()
-        val num1=random.nextInt(10)
-        val num2=random.nextInt(10)
+        val num1=Random().nextInt(10)
+        val num2=Random().nextInt(10)
         val operacion = "$num1 + $num2"
         operacionSiguiente.text=operacion
+        var resop=num1+num2
     }
 
-    private fun operacionAntigua(){
-        operacionAntigua.text=operacionAzar.text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fun animacion(view:View,tiempoX:Long,tiempoY:Long){
+        view.animate().apply {
+            scaleX(0.9f)
+            scaleY(0.9f)
+            duration=tiempoX
+        }.withEndAction{
+            view.animate().apply {
+                scaleX(1.0f)
+                scaleY(1.0f)
+                duration=tiempoY
+            }
+        }
+    }
+
+
+    fun ajustes(view: View) {
+        animacion(view,150,150)
     }
 }
